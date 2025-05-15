66bcd3595188e5546f620079506e18f6a201f204
Jasper <dev@jasperstolwijk.nl>
2025-05-15 13:51:12 +0200

    Corepack support
    Enabled corepack support for the container, if you run into errors running this. Make sure to put the yarn version in the package.json

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 node_version=$(head -n 1 .nvmrc | tr '[:upper:]' '[:lower:]'| sed 's:.*/::')
 node_image="node:${node_version}-alpine"
 
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app "${node_image}" yarn
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app "${node_image}" yarn run "$1"
+
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app "${node_image}" sh -c "corepack enable && yarn && yarn run \"$1\""

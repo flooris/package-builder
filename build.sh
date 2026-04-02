297976a8b0e95d737f951c5661907c22f7ed64c4
SanderFlooris <s.bish@flooris.nl>
2026-04-02 10:16:22 +0200

    Fixed issue that caused build assets to have the wrong permissions set

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 node_version=$(head -n 1 .nvmrc | tr '[:upper:]' '[:lower:]'| sed 's:.*/::')
 node_image="node:${node_version}-alpine"
 
-
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app "${node_image}" sh -c "corepack enable && yarn && yarn run \"$1\""
+docker run --rm \
+  --user "$(id -u):$(id -g)" \
+  -e HOME=/tmp \
+  -v "$PWD":/usr/src/app \
+  -w /usr/src/app \
+  "${node_image}" \
+  sh -c "mkdir -p /tmp/bin && corepack enable --install-directory /tmp/bin && export PATH=\"/tmp/bin:\$PATH\" && yarn && yarn run \"$1\""

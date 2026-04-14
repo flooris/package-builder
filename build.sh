32d3c7fc551350ad3db3c6f288f86286ce0c0abb
SanderFlooris <s.bish@flooris.nl>
2026-04-14 15:00:29 +0200

    Fixed permission denied issue on Docker rootless installs

--- build.sh
+++ build.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
+set -e
+
 node_version=$(head -n 1 .nvmrc | tr '[:upper:]' '[:lower:]'| sed 's:.*/::')
 node_image="node:${node_version}-alpine"
 
+# Check if the Docker engine is running in rootless mode
+if docker info --format '{{.SecurityOptions}}' | grep -iq "rootless"; then
+    echo "[flooris-package-builder] Docker is running in ROOTLESS mode."
+    IS_ROOTLESS=true
+else
+    echo "[flooris-package-builder] Docker is running in STANDARD mode."
+    IS_ROOTLESS=false
+fi
+
+user_flag=()
+if [ "$IS_ROOTLESS" = false ]; then
+  # We are not running in rootless mode, use the user and group ID of the current user instead
+  # This should prevent us from writing the node_modules and compiled assets as the Docker/root user
+  user_flag=(--user "$(id -u):$(id -g)")
+fi
+
 docker run --rm \
-  --user "$(id -u):$(id -g)" \
+  "${user_flag[@]}" \
   -e HOME=/tmp \
   -v "$PWD":/usr/src/app \
   -w /usr/src/app \
